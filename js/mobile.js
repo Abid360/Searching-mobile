518da52fe109bed08050dde96236f1a176ed1fcd
Ifranul Hoque <iamabid360@gmail.com>
2022-10-09 23:10:37 +0600

    card and image load done

--- js/mobile.js
+++ js/mobile.js
@@ -8,5 +8,26 @@ const searchMobile = () => {
   // console.log(url);
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data.data));
+    .then((data) => displaySearchResults(data.data));
+};
+
+const displaySearchResults = (mobiles) => {
+  const SearchResult = document.getElementById("search-result");
+  mobiles.forEach((mobile) => {
+    console.log(mobile);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card h-100">
+          <img src="${mobile.image}" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">${mobile.brand}</h5>
+            <p class="card-text">
+              ${mobile.phone_name}
+            </p>
+          </div>
+        </div>
+    `;
+    SearchResult.appendChild(div);
+  });
 };
