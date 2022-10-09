0843d4b3260f82ab0f637e43c4a13ee057ebdbe5
Ifranul Hoque <iamabid360@gmail.com>
2022-10-09 19:14:30 +0600

    initial commit

---
+++ js/mobile.js
@@ -0,0 +1,12 @@
+const searchMobile = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  //clear data
+  // searchField.value = "";
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+  // console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data.data));
+};
